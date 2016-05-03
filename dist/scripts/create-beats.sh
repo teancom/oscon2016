51b1167bf09bfe83540da4077f9349785463c7e8
bryanl <bryanliles@gmail.com>
2016-05-03 11:21:28 -0400

    create beats scripts

---
+++ dist/scripts/create-beats.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+id=$(cat /etc/project-id)
+esURL="http://app.${id}.x.pifft.com:9200"
+name=beats-dashboards-1.1.0
+
+curl -sSL ${esURL}/_template | grep '{}'
+if [[ $? == 0 ]]; then
+  echo "uploading elasticsearch templates"
+  curl -o /tmp/${name}.zip -sSL https://s3.pifft.com/oscon2016/${name}.zip
+  cd /tmp
+  unzip ${name}.zip
+  cd ${name}
+  ./load.sh -l ${esURL}
+else
+  echo "elasticsearch templates existed on server"
+fi
